b97ccd7fd51ab8602b7a46d151bf827ba05581a1
WickedJonas <100497587+WickedJonas@users.noreply.github.com>
2023-11-06 13:08:28 +0100

    Created classes and README file, plus added fields for the TrainDeparture class

---
+++ src/main/java/TrainDispatchSystem.java
@@ -0,0 +1,8 @@
+/**
+ * Train departure app, used to manage and control train departures over the course of a day.
+ * @version 1.0.0
+ * @author Jonas Ingebo, BIDATA 1
+ * @since 1 Nov, 2023
+ */
+public class TrainDispatchSystem {
+}
---
+++ src/main/java/trains/TrainDepartures.java
@@ -0,0 +1,18 @@
+package trains;
+
+/**
+ * Train departure app, used to manage and control train departures over the course of a day.
+ * @version 1.0.0
+ * @author Jonas Ingebo, BIDATA 1
+ * @since 6 Nov, 2023
+ */
+public class TrainDepartures {
+
+    private String departureTime;
+    private String line;
+    private int trainNumber;
+    private String destination;
+    private int track;
+
+
+}
